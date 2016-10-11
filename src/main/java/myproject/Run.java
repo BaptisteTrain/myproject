537f2f6446b2b69a56918c5d9377ca7af0e1b1f3
Baptiste Train <baptiste.train@gmail.com>
2016-10-11 11:53:11 +0200

    first commit

---
+++ src/main/java/myproject/Run.java
@@ -0,0 +1,13 @@
+package myproject;
+
+import com.google.common.base.Strings;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Strings.isNullOrEmpty("ok"));
+		
+	}
+	
+}
