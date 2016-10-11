17ee183d0bbb012afa9320bc8b0fb2e7325f5730
Nadir <nadir.allam@gmail.com>
2016-10-11 15:03:51 +0200

    J'ai supprimé tout les system out println

--- src/main/java/myproject/Run.java
+++ src/main/java/myproject/Run.java
@@ -6,13 +6,6 @@ public class Run {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(Strings.isNullOrEmpty("ok"));
-		System.out.println("bonjour");
-		System.out.println("C'est moi !!!");
-		System.out.println("bonjour");
-		System.out.println("ok");
-		System.out.println("un push");
-		System.out.println("très bien");
 		
 	}
 	
